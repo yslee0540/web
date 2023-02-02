a0951e025dec0f67735318cdc44db540df1f587b
yslee0540 <124110627+yslee0540@users.noreply.github.com>
2023-02-02 17:33:36 +0900

    Add files via upload

---
+++ day04/out.js
@@ -0,0 +1,16 @@
+function 빨래하다 () {
+    console.log("1. 먼지를 털자.")
+    console.log("2. 물에 집어 넣는다.")
+    console.log("3. 비누를 바른다.")
+    console.log("4. 오물조물 비빈다.")
+    console.log("5. 물에 여러번 헹군다.")
+    console.log("6. 짠다.")
+    console.log("7. 넌다.")
+}
+
+function 청소하다() {
+    console.log("1. 먼지를 치운다.")
+    console.log("2. 물건을 제자리에 놓는다.")
+    console.log("3. 청소기를 사용한다.")
+    console.log("4. 걸레질을 한다.")
+}
---
+++ day04/out2.js
@@ -0,0 +1,12 @@
+function 여행하다 () {
+    console.log("1. 숙소에 간다.")
+    console.log("2. 관광지에 간다.")
+    console.log("3. 저녁식사를 한다.")
+    console.log("4. 숙소로 돌아온다.")
+}
+
+function 쉬다() {
+    console.log("1. 침대로 간다.")
+    console.log("2. 휴대폰을 켠다.")
+    console.log("3. 보고싶은 영상을 재생한다.")
+}
